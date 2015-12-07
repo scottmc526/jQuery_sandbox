02661ab44da5de6e8422ad50a43fe5cfa2ef0a5f
Scott Christensen <scottmc526@gmail.com>
2015-12-07 10:22:12 -0700

    foxes

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
+$(document).ready(function (){
+  $('#trigger').click(function() {
+      var i = Math.floor(Math.random() * collection['images'].length);
+      var result = collection['images'][i]['image_src'];
+
+      $('#container').append('<img src="'+result+'"/>');
+
+  })
+
+  $('#remove').click(function() {
+    $('#container').find('img').remove()
+  })
+
+  $('#block').click(function() {
+    $('#container').find('img').css({'display':'block'});
+  })
+
+
+
+})
+
 //instructions:
 
 // Write a script that , when you click on the button,
@@ -8,4 +29,4 @@
 // added and displayed.  Example:  a new add button would change all
 // to display: block in the existing dom and add a new random fox which is
 // also display: block.  Hit the display float button and they all change
-// back to float and it adds a new floated random fox. 
+// back to float and it adds a new floated random fox.
